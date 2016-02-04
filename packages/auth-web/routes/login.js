cd54cac2b6bf97a6d52e5723be327192c0de172f
Alpha Shuro <alphashuro@gmail.com>
2016-02-04 22:17:26 +0200

    add observable api to tag

--- packages/auth-web/routes/login.js
+++ packages/auth-web/routes/login.js
@@ -1,8 +1,9 @@
 import { routes } from 'meteor/training-manager:lib';
+import auth from '../api/auth.js';
 
 routes.public.route('/login', {
   name: 'login',
   action() {
-    RiotLayout.render('login');
+    RiotLayout.render('login', auth);
   },
 });
--- packages/auth-web/routes/signup.js
+++ packages/auth-web/routes/signup.js
@@ -1,8 +1,9 @@
 import { routes } from 'meteor/training-manager:lib';
+import auth from '../api/auth.js';
 
 routes.public.route('/signup', {
   name: 'signup',
   action: function signUpRouteAction() {
-    RiotLayout.render('signup');
+    RiotLayout.render('signup', auth);
   },
 });
