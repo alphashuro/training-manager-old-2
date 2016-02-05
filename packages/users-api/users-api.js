829b83b6127e087a98e51d07738e69838cb39a78
Alpha Shuro <alphashuro@gmail.com>
2016-02-05 09:20:45 +0200

    create User api export

--- packages/users-api/users-api.js
+++ packages/users-api/users-api.js
@@ -1,1 +1,17 @@
-import './collections/users.js';
+import usersCollections from './collections/index.js';
+import usersApi from './api/index.js';
+
+import { Collections, api } from 'meteor/training-manager:lib';
+
+_.extend( Collections, usersCollections );
+
+api.Users = usersApi;
+
+const Users = {
+  api: usersApi,
+  collections: usersCollections,
+};
+
+export { usersApi, usersCollections };
+
+export default Users;
